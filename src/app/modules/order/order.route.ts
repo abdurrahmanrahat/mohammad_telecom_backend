9b1e17819a2b4734b25194a758db3691d7463ddb
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-05-27 20:36:02 +0600

    order route

---
+++ src/app/modules/order/order.route.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import ValidateRequest from '../../middlewares/ValidateRequest';
+import { OrderControllers } from './order.controller';
+import { OrderValidations } from './order.validation';
+// import auth from '../../middlewares/auth'; // Optional
+// import { USER_ROLE } from '../../../constants/user'; // Optional
+
+const router = express.Router();
+
+router.post(
+  '/',
+  ValidateRequest(OrderValidations.createOrderValidationSchema),
+  OrderControllers.createOrder,
+);
+
+router.get(
+  '/',
+  // auth(USER_ROLE.admin),
+  OrderControllers.getAllOrders,
+);
+
+router.get(
+  '/:orderId',
+  // auth(USER_ROLE.admin, USER_ROLE.customer),
+  OrderControllers.getSingleOrder,
+);
+
+router.patch(
+  '/:orderId',
+  // auth(USER_ROLE.admin),
+  ValidateRequest(OrderValidations.updateOrderValidationSchema),
+  OrderControllers.updateOrder,
+);
+
+router.delete(
+  '/:orderId',
+  // auth(USER_ROLE.admin),
+  OrderControllers.deleteOrder,
+);
+
+export const OrderRoutes = router;
